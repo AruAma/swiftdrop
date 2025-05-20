9b61b8608b70a16c8113a329e1fc1e1a1a37bde4
MadinaAltyn <hellkitty21@mail.ru>
2025-05-21 01:19:53 +0500

    Update serializers.py

--- exchange/serializers.py
+++ exchange/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Message, File
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
