584ba152c0e29001b1784720c371b54cd95bc7f8
MadinaAltyn <hellkitty21@mail.ru>
2025-05-21 01:18:10 +0500

    Update models.py

--- exchange/models.py
+++ exchange/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
+
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
@@ -10,11 +20,16 @@ class Message(models.Model):
     def __str__(self):
         return f"{self.sender} → {self.recipient} at {self.timestamp}"
 
+
 class File(models.Model):
     file = models.FileField(upload_to='uploads/')
     uploader = models.ForeignKey(User, related_name='uploaded_files', on_delete=models.CASCADE)
     recipient = models.ForeignKey(User, related_name='received_files', on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.file.name} → {self.recipient}"
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -23,6 +38,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
-
-    def __str__(self):
-        return f"{self.file.name} → {self.recipient}"
