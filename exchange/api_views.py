f69c293f84326795c1322d9d5f2d4fd4b8b9405e
MadinaAltyn <hellkitty21@mail.ru>
2025-05-21 07:19:59 +0500

    Update api_views.py

--- exchange/api_views.py
+++ exchange/api_views.py
@@ -1,10 +1,37 @@
-from .models import Profile
-from .serializers import ProfileSerializer
-from rest_framework import generics, permissions
+from rest_framework import viewsets, generics, permissions
+from django.db.models import Q
+from .models import Message, File, Profile
+from .serializers import MessageSerializer, FileSerializer, ProfileSerializer, RegisterSerializer
 
-class ProfileDetail(generics.RetrieveUpdateAPIView):
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Message.objects.filter(Q(sender=user) | Q(recipient=user))
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+class FileViewSet(viewsets.ModelViewSet):
+    serializer_class = FileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return File.objects.filter(Q(uploader=user) | Q(recipient=user))
+
+    def perform_create(self, serializer):
+        serializer.save(uploader=self.request.user)
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user.profile
+
+class RegisterAPI(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
