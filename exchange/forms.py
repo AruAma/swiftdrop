5b46b213ca35dc519de5a9e3b886ec86adf24fe0
MadinaAltyn <hellkitty21@mail.ru>
2025-05-21 07:11:00 +0500

    Update forms.py

--- exchange/forms.py
+++ exchange/forms.py
@@ -1,5 +1,15 @@
 from django import forms
-from .models import Profile
+from .models import Message, File, Profile
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['recipient', 'content']
+
+class FileForm(forms.ModelForm):
+    class Meta:
+        model = File
+        fields = ['recipient', 'file']
 
 class ProfileForm(forms.ModelForm):
     class Meta:
