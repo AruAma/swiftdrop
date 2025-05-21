15cf4abf12d75f668b423c89c7b77fe84ae0b532
MadinaAltyn <hellkitty21@mail.ru>
2025-05-21 07:10:08 +0500

    Update models.py

--- exchange/models.py
+++ exchange/models.py
@@ -3,14 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    else:
-        instance.profile.save()
-
-
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     recipient = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
@@ -19,7 +11,6 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.sender} → {self.recipient} at {self.timestamp}"
-
 
 class File(models.Model):
     file = models.FileField(upload_to='uploads/')
@@ -30,7 +21,6 @@ class File(models.Model):
     def __str__(self):
         return f"{self.file.name} → {self.recipient}"
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
@@ -38,3 +28,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
