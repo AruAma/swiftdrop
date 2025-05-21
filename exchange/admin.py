387b84fd0367163d6f1f53cf33b6f19c2d789287
MadinaAltyn <hellkitty21@mail.ru>
2025-05-21 07:15:04 +0500

    Update admin.py

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Message, File, Profile
 
+admin.site.register(Message)
+admin.site.register(File)
 admin.site.register(Profile)
